623783a1d933f3f45a04f2e1011b2a0a3a7c9577
CristianFelipeSalCon <cristianfelipesalcon@gmail.com>
2023-09-21 14:38:22 -0500

    cambio

--- exercises7_file.py
+++ exercises7_file.py
@@ -1,7 +1,5 @@
 #Name: Cristian Felipe Salinas Contreras
-#Exercise 1: Write a program to read through a file and print 
-#the contents of the file (line by line) are all in upper case. Executing 
-#the program will look as follows:
+print("Exercise 1: Write a program to read through a file and print the contents of the file (line by line) are all in upper case. Executing the program will look as follows:")
 mboxshort=open("mbox-short.txt")
 mboxshort=mboxshort.read()
 mboxshort=mboxshort.upper()
