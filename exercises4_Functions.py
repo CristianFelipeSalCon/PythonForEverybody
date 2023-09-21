ce933322013e23bf3087fa8f9de1a1fbc02690d3
Cristian Felipe Salinas Contreras <144187311+CristianFelipeSalCon@users.noreply.github.com>
2023-09-21 13:04:19 -0500

    Agregue mi nombre

--- exercises4_Functions.py
+++ exercises4_Functions.py
@@ -1,4 +1,4 @@
-
+#Cristian Felipe Salinas Contreras
 print("Exercise 1: Run the program on your system and see what numbers you get. Run the program more than once and see what numbers you get.")
 import math
 import random
